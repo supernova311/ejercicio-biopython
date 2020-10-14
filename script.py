a7a17bea42599c1a2afec22cb641e08b23484e07
supernova311 <70219331+supernova311@users.noreply.github.com>
2020-10-14 14:07:34 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 from Bio import SeqIO
 from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
+import os.path
 def summarize_contents(filename):
   record = SeqIO.read(filename, "genbank")
-  print("Name: ", record.name)
+        print("Name: ", record.name)
+        print("Path: ", os.path.dirname(filename))
   for seq_record in SeqIO.parse(filename,"genbank"):
         print("ID:",record.id)
   summarize_contents(filename)
