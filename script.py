fc1a1f8d07a35ecbb65cb89e300825cdc8069377
supernova311 <70219331+supernova311@users.noreply.github.com>
2020-10-27 03:00:51 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,17 @@
 from Bio import SeqIO
 from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
-import os.path
+import os
+
+filename= /home/supernova/Documents/biopython-notebook/notebooks/data/ls_orchid.gbk
 def summarize_contents(filename):
-  record = SeqIO.read(filename, "genbank")
-        print("Name: ", record.name)
-        print("Path: ", os.path.dirname(filename))
-  record =list(SequIO.parse(filename, "genbank"))
-        print("Num_records: "%len (records))
-  for seq_record in SeqIO.parse(filename,"genbank")
-        print("ID:",record.id)
-        print("Name: ", record.name)
-        print("Description: ", record.description)
+        records= list(SeqIO.parse(filename, "genbank"))
+        print ("Path: ", os.path.dirname(filename))
+        print ("num_records = %i records" % len(records))
+        print ("n\n")
+        for seq_record in SeqIO.parse(filename, "genbank"):
+                print("Name: ", seq_record.name)
+                print("ID: ", seq_record.id)
+                print(Description: ", seq_record.descrition)
+
   summarize_contents(filename)
