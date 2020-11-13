efc8dd3af53bcf7326145660fcfaeb982cc046d5
supernova <danavazsa@gmail.com>
2020-11-13 00:42:20 -0700

    Agrega nueva funcion a script.py. Funcion que genera el complemento de inverso de dos cadenas. [biophyton-tarea3-ex1]

--- script.py
+++ script.py
@@ -3,18 +3,28 @@ from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
 import os.path
 
-filename = "/home/supernova/Desktop/biopython-notebook/notebooks/data/ls_orchid.gbk"
+filename = "/mnt/c/Users/Arian Vázquez/Desktop/ejercicio-biopython/data/ls_orchid.gbk"
 def summarize_contents(filename):
         lista = []
         lista = os.path.split(filename)
         records= list(SeqIO.parse(filename, "genbank"))
         cadena  = ("file: " + lista[1])
         cadena += ("path: " + lista[2])
-        cadena += (num_records = %i records" % len(records))
+        cadena += ("num_records = %i records" % len (records))
         for seq_record in SeqIO.parse(filename, "genbank"):
                 cadena += ("Name: ", seq_record.name)
                 cadena += ("ID: ", seq_record.id)
-                cadena += (Description: ", + str (seq_record.descrition))
-              print(cadena)
+                cadena += ("Description: " + str (seq_record.descrition))
+        print(cadena)
 
-  summarize_contents(filename)
+from Bio.Seq import Seq
+
+list_of_seqs = [Seq("acgtaggttaccatcatg"), Seq("cgtgattggcctagccgt")]
+def concatenate_and_get_reverse_of_complement():          
+        concatenated = Seq("")
+        for s in list_of_seqs():
+                concatenated += s
+        concatenated
+        my_seq = concatenated
+        print(my_seq.reverse_complement())
+
